c6140a76f40d772cfffd0f0c6e1885074afe8fbd
Paul Craig <paul@pcraig3.ca>
2022-11-27 18:00:24 -0500

    Make the favicon happen

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,12 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Gothic+A1:400,600&amp;display=fallback"
           />
+
+          {/* Favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
